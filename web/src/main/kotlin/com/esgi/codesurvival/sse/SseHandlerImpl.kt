4c89334a40d213f08321eb58a39953228102de35
Simon <halimisimon@gmail.com>
2021-07-04 15:19:48 +0200

    [ADD] Stabilize and correct ghost emitter in SSE

--- web/src/main/kotlin/com/esgi/codesurvival/sse/SseHandlerImpl.kt
+++ web/src/main/kotlin/com/esgi/codesurvival/sse/SseHandlerImpl.kt
@@ -10,58 +10,69 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter.event
 
 @Component
 @EnableScheduling
-class SseHandlerImpl: SseHandler {
+class SseHandlerImpl : SseHandler {
 
     val gameEventEmitters: HashMap<String, MutableList<SseEmitter>> = HashMap()
+    val ghostEmitters: HashMap<String, MutableList<SseEmitter>> = HashMap()
 
     override fun subscribeToGameEventListening(id: String): SseEmitter? {
         val emitter = SseEmitter(-1L)
+
         if (!gameEventEmitters.containsKey(id)) {
             gameEventEmitters[id] = mutableListOf()
         }
 
         gameEventEmitters[id]?.add(emitter)
-        println(gameEventEmitters)
-
-        emitter.onCompletion {
-            this.gameEventEmitters.get(id)?.remove(emitter)
-            println("remove successful")
-        }
-
         return emitter
     }
 
 
     override fun emitGameEventTo(userId: String, gameEventSerialized: String) {
-        println(gameEventEmitters)
         if (gameEventEmitters.containsKey(userId)) {
             for (emitter in gameEventEmitters[userId]!!) {
                 try {
-                     val event = event()
-                    event.data(gameEventSerialized)
-                    println(event)
-                    emitter.send(event)
+                    this.emit(emitter, userId, gameEventSerialized)
                 } catch (e: Error) {
                     println(e)
                 }
             }
         }
+        this.cleanEmitters()
+    }
+
+    private fun cleanEmitters() {
+        for ((key, value) in ghostEmitters) {
+            for (emitter in value) {
+                this.gameEventEmitters[key]?.remove(emitter)
+            }
+        }
+
+        this.ghostEmitters.clear()
     }
 
     @Scheduled(fixedRate = 30000)
     fun keepAlive() {
-        println("yulu")
         for ((key, value) in gameEventEmitters) {
             for (emitter in value) {
-                println(value)
-                val event = event()
-                println("kill the bitch")
-                event.data("duguuuu")
-                emitter.send(event)
+                this.emit(emitter, key, "")
             }
         }
-
+        this.cleanEmitters()
     }
 
+
+    private fun emit(emitter: SseEmitter, userId: String, emission: String) {
+        val event = event()
+        event.data(emission)
+
+        kotlin.runCatching {
+            emitter.send(event)
+        }.onFailure {
+            if (!ghostEmitters.containsKey(userId)) {
+                ghostEmitters[userId] = mutableListOf()
+            }
+            ghostEmitters[userId]?.add(emitter)
+        }
+    }
 
 }
