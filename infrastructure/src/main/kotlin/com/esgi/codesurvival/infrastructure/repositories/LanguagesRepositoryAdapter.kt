24015d4dcf27bea1cdf99e07354ebea5c407a06e
Simon <halimisimon@gmail.com>
2021-07-21 10:00:37 +0200

    [ADD] annotate transactional method on repositories

--- infrastructure/src/main/kotlin/com/esgi/codesurvival/infrastructure/repositories/LanguagesRepositoryAdapter.kt
+++ infrastructure/src/main/kotlin/com/esgi/codesurvival/infrastructure/repositories/LanguagesRepositoryAdapter.kt
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.util.*
+import javax.transaction.Transactional
 
 @Service
 class LanguagesRepositoryAdapter @Autowired constructor(
--- infrastructure/src/main/kotlin/com/esgi/codesurvival/infrastructure/repositories/UsersRepositoryAdapter.kt
+++ infrastructure/src/main/kotlin/com/esgi/codesurvival/infrastructure/repositories/UsersRepositoryAdapter.kt
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.util.*
+import javax.transaction.Transactional
 
 
 @Service
